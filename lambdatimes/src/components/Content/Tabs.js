4009439a150588018acfccd734bf4f88d325308e
unknown <robertrey1234@gmail.com>
2019-06-07 10:53:40 -0500

    started on getting content displayed to screen

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,10 +7,16 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {tabs.map(tab => (<Tab selectTabHandler={selectTabHandler} selectedTab={selectedTab} tab={tab} />
+            ))}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.array,
+  selectedTab: PropTypes.string,
+}
 export default Tabs;
